39579fba2e849b573d72a15df7c5fe357516e55e
Eduardo Vieira <bolacha0953@gmail.com>
2025-11-05 04:39:36 +0100

    pagina 404

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+
 app.use(express.json());
+
 
 
 app.get("/", (req, res) => {
   res.send("Servidor Node.js funcionando 🚀");
+});
+
+app.use(function(req,res,next){
+    console.log("[SYSTEM] User reached 404");
+    res.status(404);
+    res.sendFile(path.join(__dirname + "/public/404_static.html"));
 });
 
 app.listen(PORT, () => {
